8055ee8f79b0c63244349f45d18a00ac70f10c6d
Muhammad Sami Tariq <samitariq40@gmail.com>
2024-03-06 17:26:19 +0500

    Added a component

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+// function sum(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+
+import React from "react";
+
+// module.exports = sum;
+
+// export * from './components';
+
+export const MyComponent = () => {
+  return <div>Hello word</div>;
+};
---
+++ types/components/HelpButton/HelpButton.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export interface ButtonProps {
+    label: string;
+}
+declare const HelpButton: (props: ButtonProps) => React.JSX.Element;
+export default HelpButton;
