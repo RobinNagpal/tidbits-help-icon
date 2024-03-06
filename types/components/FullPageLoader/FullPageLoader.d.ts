0a904e4afb5d7be71c00ccf250970a144a26dbca
Muhammad Sami Tariq <samitariq40@gmail.com>
2024-03-06 22:21:11 +0500

    some cleanup

--- types/components/FullPageLoader/FullPageLoader.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-type FullPageLoaderProps = {
-    message?: string;
-};
-export default function FullPageLoader({ message }: FullPageLoaderProps): React.JSX.Element;
-export {};
--- types/components/HelpButton/HelpButton.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface HelpButtonProps {
-    showModal: (arg: boolean) => void;
-}
-export default function HelpButton({ showModal }: HelpButtonProps): React.JSX.Element;
-export {};
