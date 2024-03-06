bbac2a6219807e2d373dddfb1dd0f44acf2ddc5e
Muhammad Sami Tariq <samitariq40@gmail.com>
2024-03-06 17:36:46 +0500

    some changes

--- build.js
+++ build.js
@@ -1,44 +1,44 @@
-// const { build } = require("esbuild");
-// const { dependencies, peerDependencies } = require("./package.json");
-// const { Generator } = require("npm-dts");
+const { build } = require('esbuild');
+const { dependencies, peerDependencies } = require('./package.json');
+const { Generator } = require('npm-dts');
 
-// new Generator({
-//   entry: "src/index.ts",
-//   output: "dist/index.d.ts",
-// }).generate();
+new Generator({
+  entry: 'src/index.txs',
+  output: 'dist/index.d.ts',
+}).generate();
 
-// const sharedConfig = {
-//   entryPoints: ["src/index.ts"],
-//   bundle: true,
-//   minify: true,
-//   external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
-// };
+const sharedConfig = {
+  entryPoints: ['src/index.tsx'],
+  bundle: true,
+  minify: true,
+  external: Object.keys(dependencies).concat(Object.keys(peerDependencies)),
+};
 
-// build({
-//   ...sharedConfig,
-//   platform: "node", // for CJS
-//   outfile: "dist/index.js",
-// });
+build({
+  ...sharedConfig,
+  platform: 'node', // for CJS
+  outfile: 'dist/index.js',
+});
 
-// build({
-//   ...sharedConfig,
-//   outfile: "dist/index.esm.js",
-//   platform: "neutral", // for ESM
-//   format: "esm",
-// });
+build({
+  ...sharedConfig,
+  outfile: 'dist/index.esm.js',
+  platform: 'neutral', // for ESM
+  format: 'esm',
+});
 
-const esbuild = require('esbuild');
-const { nodeExternalsPlugin } = require('esbuild-node-externals');
-esbuild
-  .build({
-    entryPoints: ['./src/index.tsx'],
-    outfile: 'dist/index.js',
-    bundle: true,
-    minify: true,
-    treeShaking: true,
-    platform: 'node',
-    format: 'cjs',
-    target: 'node14',
-    plugins: [nodeExternalsPlugin()],
-  })
-  .catch(() => process.exit(1));
+// const esbuild = require('esbuild');
+// const { nodeExternalsPlugin } = require('esbuild-node-externals');
+// esbuild
+//   .build({
+//     entryPoints: ['./src/index.tsx'],
+//     outfile: 'dist/index.js',
+//     bundle: true,
+//     minify: true,
+//     treeShaking: true,
+//     platform: 'node',
+//     format: 'cjs',
+//     target: 'node14',
+//     plugins: [nodeExternalsPlugin()],
+//   })
+//   .catch(() => process.exit(1));
